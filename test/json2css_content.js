e5eef89fc31a793e3c1a9a8229299d325fa2a71b
Todd Wolfson <todd@twolfson.com>
2013-06-22 00:12:30 -0700

    Fixed json2css to output total_width and tests now passing

--- test/json2css_content.js
+++ test/json2css_content.js
@@ -10,16 +10,16 @@ module.exports = {
   'An array of image positions, dimensions, and names': function () {
     // TODO: The malicious URL should be placed in a separate test but I want to test every engine and hate the bloat
     this.info = [
-      {'name': 'sprite1', 'x': 0, 'y': 0, 'width': 10, 'height': 20, 'total-width': 80, 'total-height': 100, 'image': 'nested/dir/spritesheet.png'},
-      {'name': 'sprite2', 'x': 10, 'y': 20, 'width': 20, 'height': 30, 'total-width': 80, 'total-height': 100, 'image': 'nested/dir/spritesheet.png'},
-      {'name': 'sprite3', 'x': 30, 'y': 50, 'width': 50, 'height': 50, 'total-width': 80, 'total-height': 100, 'image': 'nested/dir/( \'")/spritesheet.png'}
+      {'name': 'sprite1', 'x': 0, 'y': 0, 'width': 10, 'height': 20, 'total_width': 80, 'total_height': 100, 'image': 'nested/dir/spritesheet.png'},
+      {'name': 'sprite2', 'x': 10, 'y': 20, 'width': 20, 'height': 30, 'total_width': 80, 'total_height': 100, 'image': 'nested/dir/spritesheet.png'},
+      {'name': 'sprite3', 'x': 30, 'y': 50, 'width': 50, 'height': 50, 'total_width': 80, 'total_height': 100, 'image': 'nested/dir/( \'")/spritesheet.png'}
     ];
   },
   'An object of image positions and dimensions keyed by names': function () {
     this.info = {
-      'sprite1': {'x': 0, 'y': 0, 'width': 10, 'height': 20, 'total-width': 80, 'total-height': 100, 'image': 'nested/dir/spritesheet.png'},
-      'sprite2': {'x': 10, 'y': 20, 'width': 20, 'height': 30, 'total-width': 80, 'total-height': 100, 'image': 'nested/dir/spritesheet.png'},
-      'sprite3': {'x': 30, 'y': 50, 'width': 50, 'height': 50, 'total-width': 80, 'total-height': 100, 'image': 'nested/dir/( \'")/spritesheet.png'}
+      'sprite1': {'x': 0, 'y': 0, 'width': 10, 'height': 20, 'total_width': 80, 'total_height': 100, 'image': 'nested/dir/spritesheet.png'},
+      'sprite2': {'x': 10, 'y': 20, 'width': 20, 'height': 30, 'total_width': 80, 'total_height': 100, 'image': 'nested/dir/spritesheet.png'},
+      'sprite3': {'x': 30, 'y': 50, 'width': 50, 'height': 50, 'total_width': 80, 'total_height': 100, 'image': 'nested/dir/( \'")/spritesheet.png'}
     };
   },
   'processed via json2css': function () {
@@ -30,8 +30,8 @@ module.exports = {
     this.result = result;
 
     // If we are debugging, output results to a file
-    // if (true) {
-    if (false) {
+    if (true) {
+    // if (false) {
       try { fs.mkdirSync(__dirname + '/actual_files/'); } catch (e) {}
       fs.writeFileSync(__dirname + '/actual_files/' + this.filename, result, 'utf8');
     }
