66f9e478073776f4b8335c3de71837e418661260
Todd Wolfson <todd@twolfson.com>
2013-11-29 03:10:16 -0800

    Moved to common image setup

--- test/json2css_scss_test.js
+++ test/json2css_scss_test.js
@@ -4,14 +4,7 @@ var assert = require('assert'),
     utils = require('./utils');
 
 describe('An array of image positions, dimensions, and names', function () {
-  before(function () {
-    // TODO: The malicious URL should be placed in a separate test but I want to test every engine and hate the bloat
-    this.info = [
-      {'name': 'sprite1', 'x': 0, 'y': 0, 'width': 10, 'height': 20, 'total_width': 80, 'total_height': 100, 'image': 'nested/dir/spritesheet.png'},
-      {'name': 'sprite2', 'x': 10, 'y': 20, 'width': 20, 'height': 30, 'total_width': 80, 'total_height': 100, 'image': 'nested/dir/spritesheet.png'},
-      {'name': 'sprite3', 'x': 30, 'y': 50, 'width': 50, 'height': 50, 'total_width': 80, 'total_height': 100, 'image': 'nested/dir/( \'")/spritesheet.png'}
-    ];
-  });
+  utils.setupImages();
 
   describe('processed by `json2css` into SCSS', function () {
     before(function () {
--- test/utils/index.js
+++ test/utils/index.js
@@ -26,3 +26,14 @@ exports.assertMatchesAsExpected = function () {
     assert.strictEqual(actual, expected);
   });
 };
+
+exports.setupImages = function () {
+  before(function () {
+    // TODO: The malicious URL should be placed in a separate test but I want to test every engine and hate the bloat
+    this.info = [
+      {'name': 'sprite1', 'x': 0, 'y': 0, 'width': 10, 'height': 20, 'total_width': 80, 'total_height': 100, 'image': 'nested/dir/spritesheet.png'},
+      {'name': 'sprite2', 'x': 10, 'y': 20, 'width': 20, 'height': 30, 'total_width': 80, 'total_height': 100, 'image': 'nested/dir/spritesheet.png'},
+      {'name': 'sprite3', 'x': 30, 'y': 50, 'width': 50, 'height': 50, 'total_width': 80, 'total_height': 100, 'image': 'nested/dir/( \'")/spritesheet.png'}
+    ];
+  });
+};
