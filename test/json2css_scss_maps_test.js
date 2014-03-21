88fff6fb5b75d902202f591165def2a4789bb7bc
Leevi Graham <info@leevigraham.com>
2014-03-21 18:06:27 +1100

    Added scssMap template for sass 3.3+

---
+++ test/json2css_scss_maps_test.js
@@ -0,0 +1,62 @@
+var assert = require('assert'),
+    exec = require('child_process').exec,
+    Tempfile = require('temporary/lib/file'),
+    utils = require('./utils');
+
+describe('An array of image positions, dimensions, and names', function () {
+  utils.setupImages();
+
+  describe('processed by `json2css` into SCSS (Maps)', function () {
+    before(function () {
+      this.options = {'format': 'scss_maps'};
+      this.filename = 'scss_maps.scss';
+    });
+    utils.runJson2Css();
+
+    before(function writeScssToFile () {
+      // Add some SCSS to our result
+      var scssStr = this.result;
+      scssStr += '\n' + [
+        '.feature {',
+        '  height: sprite("sprite1", "height");',
+        '  @include sprite-width("sprite2");',
+        '  @include sprite-image("sprite3");',
+        '}',
+        '',
+        '.feature2 {',
+        '  @include sprite("sprite2");',
+        '}'
+      ].join('\n');
+
+      // Save the SCSS to a file for processing
+      var tmp = new Tempfile();
+      tmp.writeFileSync(scssStr);
+      this.tmp = tmp;
+    });
+    after(function () {
+      this.tmp.unlinkSync();
+    });
+
+    utils.assertMatchesAsExpected();
+
+    describe('processed by `sass --scss` (ruby) into CSS', function () {
+      // Process the SCSS
+      before(function (done) {
+        var that = this;
+        exec('sass --scss ' + this.tmp.path, function (err, css, stderr) {
+          // Assert no errors during conversion
+          assert.strictEqual(stderr, '');
+          assert.strictEqual(err, null);
+          assert.notEqual(css, '');
+
+          // Save CSS for later and callback
+          that.css = css;
+          done(err);
+        });
+      });
+
+      // Assert agains the generated CSS
+      utils.assertValidCss();
+    });
+  });
+});
