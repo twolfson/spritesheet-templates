d6f23054784e8c4d4955b648adb575e71e3eec91
Todd Wolfson <todd@twolfson.com>
2014-05-24 18:15:33 -0700

    Corrected bad test

--- test/json2css_scss_maps_test.js
+++ test/json2css_scss_maps_test.js
@@ -18,13 +18,13 @@ describe('An array of image positions, dimensions, and names', function () {
       var scssStr = this.result;
       scssStr += '\n' + [
         '.feature {',
-        '  height: sprite("sprite1", "height");',
-        '  @include sprite-width("sprite2");',
-        '  @include sprite-image("sprite3");',
+        '  height: map-get($sprite1, "height");',
+        '  @include sprite-width($sprite2);',
+        '  @include sprite-image($sprite3);',
         '}',
         '',
         '.feature2 {',
-        '  @include sprite("sprite2");',
+        '  @include sprite($sprite2);',
         '}'
       ].join('\n');
 
