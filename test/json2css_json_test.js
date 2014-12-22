a0241f9cecdeffc7880b424f92505e60c55a3e74
Todd Wolfson <todd@twolfson.com>
2014-12-22 15:28:44 -0600

    Broke out CSS tests

--- test/json2css_json_test.js
+++ test/json2css_json_test.js
@@ -32,19 +32,4 @@ describe('An array of image positions, dimensions, and names', function () {
     utils.assertMatchesAsExpected();
     assertValidJson();
   });
-
-  describe('processed by `json2css` into CSS', function () {
-    before(function () {
-      this.options = null;
-      this.filename = 'css.css';
-    });
-    utils.runJson2Css();
-
-    utils.assertMatchesAsExpected();
-    utils.runFakeJigsaw();
-    it('is valid CSS', function (done) {
-      var css = this.result;
-      utils._assertValidCss(css, done);
-    });
-  });
 });
