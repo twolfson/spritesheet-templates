9965eb425a92dca96dbfc3c8c54ccc8272c0b4e2
Todd Wolfson <todd@twolfson.com>
2014-12-22 20:24:26 -0600

    Updated input and tests mostly green

--- test/utils/index.js
+++ test/utils/index.js
@@ -11,16 +11,20 @@ var templater = require('../../');
 exports.setupImages = function () {
   before(function () {
     // TODO: The malicious URL should be placed in a separate test but I want to test every engine and hate the bloat
-    this.info = [{
-      name: 'sprite1', x: 0, y: 0, width: 10, height: 20,
-      total_width: 80, total_height: 100, image: 'nested/dir/spritesheet.png'
-    }, {
-      name: 'sprite2', x: 10, y: 20, width: 20, height: 30,
-      total_width: 80, total_height: 100, image: 'nested/dir/spritesheet.png'
-    }, {
-      name: 'sprite3', x: 30, y: 50, width: 50, height: 50,
-      total_width: 80, total_height: 100, image: 'nested/dir/( \'")/spritesheet.png'
-    }];
+    this.info = {
+      items: [{
+        name: 'sprite1', x: 0, y: 0, width: 10, height: 20,
+      }, {
+        name: 'sprite2', x: 10, y: 20, width: 20, height: 30,
+      }, {
+        name: 'sprite3', x: 30, y: 50, width: 50, height: 50,
+      }],
+      spritesheet: {
+        // TODO: Add test cases for this
+        // width: 80, height: 100, image: 'nested/dir/( \'")/spritesheet.png'
+        width: 80, height: 100, image: 'nested/dir/spritesheet.png'
+      }
+    };
   });
 };
 
