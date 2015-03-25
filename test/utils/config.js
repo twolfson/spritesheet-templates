1f806e0118ac0a4a17d15a9dc09993d8b85b5f9a
Todd Wolfson <todd@twolfson.com>
2015-03-25 00:50:59 -0500

    Fixed up names

--- test/utils/config.js
+++ test/utils/config.js
@@ -22,21 +22,21 @@ exports.multipleSprites = {
 
 exports.retinaMultipleSprites = {
   sprites: [{
-    name: 'sprite-dash-case', x: 0, y: 0, width: 10, height: 20
+    name: 'sprite-dash-case-normal', x: 0, y: 0, width: 10, height: 20
   }, {
-    name: 'sprite_snake_case', x: 10, y: 20, width: 20, height: 30
+    name: 'sprite_snake_case_normal', x: 10, y: 20, width: 20, height: 30
   }, {
-    name: 'spriteCamelCase', x: 30, y: 50, width: 50, height: 50
+    name: 'spriteCamelCaseNormal', x: 30, y: 50, width: 50, height: 50
   }],
   spritesheet: {
     width: 80, height: 100, image: 'nested/dir/spritesheet.png'
   },
   retinaSprites: [{
-    name: 'sprite-dash-case', x: 0, y: 0, width: 20, height: 40
+    name: 'sprite-dash-case-retina', x: 0, y: 0, width: 20, height: 40
   }, {
-    name: 'sprite_snake_case', x: 20, y: 40, width: 40, height: 60
+    name: 'sprite_snake_case_retina', x: 20, y: 40, width: 40, height: 60
   }, {
-    name: 'spriteCamelCase', x: 60, y: 100, width: 100, height: 100
+    name: 'spriteCamelCaseRetina', x: 60, y: 100, width: 100, height: 100
   }],
   retinaSpritesheet: {
     width: 160, height: 200, image: 'nested/dir/spritesheet.png'
