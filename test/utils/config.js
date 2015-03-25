bb0847cf46b78e562815a85f990ae7d27573fa2b
Todd Wolfson <todd@twolfson.com>
2015-03-25 00:50:11 -0500

    Added new config for retina

--- test/utils/config.js
+++ test/utils/config.js
@@ -19,3 +19,26 @@ exports.multipleSprites = {
     width: 80, height: 100, image: 'nested/dir/spritesheet.png'
   }
 };
+
+exports.retinaMultipleSprites = {
+  sprites: [{
+    name: 'sprite-dash-case', x: 0, y: 0, width: 10, height: 20
+  }, {
+    name: 'sprite_snake_case', x: 10, y: 20, width: 20, height: 30
+  }, {
+    name: 'spriteCamelCase', x: 30, y: 50, width: 50, height: 50
+  }],
+  spritesheet: {
+    width: 80, height: 100, image: 'nested/dir/spritesheet.png'
+  },
+  retinaSprites: [{
+    name: 'sprite-dash-case', x: 0, y: 0, width: 20, height: 40
+  }, {
+    name: 'sprite_snake_case', x: 20, y: 40, width: 40, height: 60
+  }, {
+    name: 'spriteCamelCase', x: 60, y: 100, width: 100, height: 100
+  }],
+  retinaSpritesheet: {
+    width: 160, height: 200, image: 'nested/dir/spritesheet.png'
+  }
+};
