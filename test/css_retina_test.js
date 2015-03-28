4e28dfb083d8a67213630110130e79dc404fbdf7
Todd Wolfson <todd@twolfson.com>
2015-03-27 21:10:01 -0500

    Starting on CSS jam

---
+++ test/css_retina_test.js
@@ -0,0 +1,16 @@
+var configUtils = require('./utils/config');
+var testUtils = require('./utils/test');
+
+describe('An retina array of image positions, dimensions, and names', function () {
+  testUtils.setInfo(configUtils.retinaMultipleSprites);
+
+  describe.only('processed by `spritesheet-templates` into retina CSS', function () {
+    testUtils.runTemplater({format: 'css_retina'});
+    testUtils.assertOutputMatches(__dirname + '/expected_files/css_retina.scss');
+
+    it('is valid CSS', function (done) {
+      var css = this.result;
+      testUtils._assertValidCss(css, done);
+    });
+  });
+});
