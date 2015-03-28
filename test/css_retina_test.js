21264de0140b2a05cae1c65b7cc49cb1458f4bb0
Todd Wolfson <todd@twolfson.com>
2015-03-27 23:36:04 -0500

    Added back full test suite

--- test/css_retina_test.js
+++ test/css_retina_test.js
@@ -4,12 +4,15 @@ var testUtils = require('./utils/test');
 describe('An retina array of image positions, dimensions, and names', function () {
   testUtils.setInfo(configUtils.retinaMultipleSprites);
 
-  describe.skip('processed by `spritesheet-templates` into retina CSS', function () {
+  describe('processed by `spritesheet-templates` into retina CSS', function () {
     testUtils.runTemplater({format: 'css_retina'});
     testUtils.assertOutputMatches(__dirname + '/expected_files/css_retina.css');
 
-    it.skip('is valid CSS', function (done) {
+    testUtils.runFakeJigsaw();
+    it('is valid CSS', function (done) {
       var css = this.result;
+      // DEV: Repair vendor specific validation issues
+      css = css.replace(/\(-webkit-min-device-pixel-ratio: 2\),/g, '');
       testUtils._assertValidCss(css, done);
     });
   });
